1e521f970cfd445f06c2fc19521fa1bc6e9bf5b2
Cornelius <neliusdesign03@gmail.com>
2026-05-05 12:06:33 +0200

    Add files via upload

---
+++ blog-posts.js
@@ -0,0 +1,23 @@
+const blogPosts = [
+  {
+    title: "New Portfolio Launch",
+    image: "images/news1.jpg",
+    Date: "June 15, 2024",
+    excerpt: "Discover our latest projects and creative endeavors in our newly launched portfolio.",
+    url: "blog.html#new-portfolio-launch"
+  },
+  {
+    title: "Design Workshop",
+    image: "images/news2.jpg",
+    Date: "July 20, 2024",
+    excerpt: "Join us for an exclusive design workshop where we share insights and techniques.",
+    url: "blog.html#design-workshop"
+  },
+  {
+    title: "Client Success Story",
+    image: "images/news3.jpg",
+    Date: "August 5, 2024",
+    excerpt: "Learn how we helped a client achieve their goals through innovative design solutions.",
+    url: "blog.html#client-success-story"
+  }
+];
